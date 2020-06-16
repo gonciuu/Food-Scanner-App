079429903629c2c9e5d07c1cfa226af055588d02
andryue <kacperwojak17@gmail.com>
2020-06-16 10:47:08 +0200

    create layout design and add to navigation component

--- app/src/main/java/com/example/foodpoint/MainActivity.kt
+++ app/src/main/java/com/example/foodpoint/MainActivity.kt
@@ -12,29 +12,6 @@ import kotlinx.android.synthetic.main.fragment_welcome.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_welcome)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        val listOfPagerCard = arrayListOf<ViewPagerCard>(
-            ViewPagerCard("Welcome to Food Point","Please give access your camera so that we can scan and provide you that what\nis inside the food"),
-            ViewPagerCard("Simply get food data","Simply scan your food barcode and get information about us ingredients\nnutrition and etc... "),
-            ViewPagerCard("Get health's nutrition","In that app you can get information about if scaned food is t\n healthy or not")
-        )
-
-        welcomeViewPager.adapter = WelcomePagerAdapter(applicationContext,listOfPagerCard)
-        welcomePagerDots.setupWithViewPager(welcomeViewPager)
-        welcomeViewPager.invalidate()
-
-
-
-
-        for (i in 0 until welcomePagerDots.tabCount) {
-            val tab = (welcomePagerDots.getChildAt(0) as ViewGroup).getChildAt(i)
-            (tab.layoutParams as ViewGroup.MarginLayoutParams).setMargins(0, 0, 30, 0);
-            tab.requestLayout()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/foodpoint/screens/WelcomeFragment.kt
+++ app/src/main/java/com/example/foodpoint/screens/WelcomeFragment.kt
@@ -5,12 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.foodpoint.R
+import com.example.foodpoint.classes.ViewPagerCard
+import com.example.foodpoint.screens.adapters.WelcomePagerAdapter
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 class WelcomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listOfPagerCard = arrayListOf<ViewPagerCard>(
+            ViewPagerCard("Welcome to Food Point","Please give access your camera so that we can scan and provide you that what\nis inside the food"),
+            ViewPagerCard("Simply get food data","Simply scan your food barcode and get information about us ingredients\nnutrition and etc... "),
+            ViewPagerCard("Get health's nutrition","In that app you can get information about if scaned food is t\n healthy or not")
+        )
+
+        welcomeViewPager.adapter = WelcomePagerAdapter(context,listOfPagerCard)
+        welcomePagerDots.setupWithViewPager(welcomeViewPager)
+        welcomeViewPager.invalidate()
+
+
+
+
+        for (i in 0 until welcomePagerDots.tabCount) {
+            val tab = (welcomePagerDots.getChildAt(0) as ViewGroup).getChildAt(i)
+            (tab.layoutParams as ViewGroup.MarginLayoutParams).setMargins(0, 0, 30, 0);
+            tab.requestLayout()
+        }
+
+        startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_scanFragment)
+        }
     }
 
 }
