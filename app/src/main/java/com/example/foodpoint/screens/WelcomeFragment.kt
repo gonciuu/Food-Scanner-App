1e6f84e6d7d4a1c4c940a85e07976430fa4d0e4b
andryue <kacperwojak17@gmail.com>
2020-06-20 16:05:45 +0200

    add camera permission request

--- app/src/main/java/com/example/foodpoint/screens/WelcomeFragment.kt
+++ app/src/main/java/com/example/foodpoint/screens/WelcomeFragment.kt
@@ -1,11 +1,15 @@
 package com.example.foodpoint.screens
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.foodpoint.R
 import com.example.foodpoint.api.food_class.Food
@@ -19,6 +23,10 @@ import kotlinx.coroutines.launch
 
 class WelcomeFragment : Fragment() {
 
+    companion object{
+        const val CAMERA_PERMISSION_REQUEST_CODE = 1234
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,11 +37,30 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setupViewPager()
         getApi()
     }
-    
+
+
+
+    private fun getCameraPermission(){
+        if(Build.VERSION.SDK_INT>=23){
+            if(ContextCompat.checkSelfPermission(requireContext(),android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                return requestPermissions(arrayOf(android.Manifest.permission.CAMERA),CAMERA_PERMISSION_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == CAMERA_PERMISSION_REQUEST_CODE){
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                getCameraPermission()
+            }
+        }
+    }
+
 
     private fun getApi() {
         var food: Food? = null
@@ -75,6 +102,7 @@ class WelcomeFragment : Fragment() {
         }
 
         startButton.setOnClickListener {
+            getCameraPermission()
             findNavController().navigate(R.id.action_welcomeFragment_to_homeFragment)
         }
 
