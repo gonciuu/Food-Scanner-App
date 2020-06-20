e72b1e30fcb9334d9533c89c1c4facdafd83eb0b
andryue <kacperwojak17@gmail.com>
2020-06-20 11:19:17 +0200

    add bottom app bar anviagtion in home fragment

--- app/src/main/java/com/example/foodpoint/screens/HomeFragment.kt
+++ app/src/main/java/com/example/foodpoint/screens/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.foodpoint.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodpoint.R
 import com.example.foodpoint.screens.adapters.recycler_views.PopularFoodAdapter
@@ -18,7 +21,38 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setRecyclerViewAdapter()
+        setBottomBarButtonsClick()
+    }
 
+
+    private fun setBottomBarButtonsClick(){
+
+        scanButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_scanFragment)
+        }
+
+        bottomAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.menuSettings->{
+                    Log.d("TAG","settings")
+                    true
+                }
+                else->{
+                    Log.d("TAG","nic")
+                    true
+                }
+            }
+        }
+
+        bottomAppBar.setNavigationOnClickListener {
+            Log.d("TAG","archiwum ")
+        }
+
+
+    }
+
+    private fun setRecyclerViewAdapter(){
         popularFoodsRecyclerView.apply {
             layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
             adapter = PopularFoodAdapter()
