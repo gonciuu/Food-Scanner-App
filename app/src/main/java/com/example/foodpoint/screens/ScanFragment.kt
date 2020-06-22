aa1fb26c36f6d10ec06758e84daa2d82c136e75f
andryue <kacperwojak17@gmail.com>
2020-06-22 12:21:30 +0200

    add getfoodinfo async from JSON when the code was scanned

--- app/src/main/java/com/example/foodpoint/screens/ScanFragment.kt
+++ app/src/main/java/com/example/foodpoint/screens/ScanFragment.kt
@@ -12,13 +12,26 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.foodpoint.R
+import com.example.foodpoint.api.food_class.Food
+import com.example.foodpoint.api.service.FoodService
+import com.example.foodpoint.api.service.RetrofitClient
 import com.example.foodpoint.screens.view_models.ScanViewModel
 import kotlinx.android.synthetic.main.fragment_scan.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 
 class ScanFragment : Fragment() {
-    private lateinit var scanViewModel:ScanViewModel
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var scanViewModel: ScanViewModel
+    private lateinit var food: Food
+    private var handler:Handler = Handler()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_scan, container, false)
     }
 
@@ -28,27 +41,14 @@ class ScanFragment : Fragment() {
 
         getBarCode()
         setupFragment()
-
     }
 
 
-    private fun getBarCode(){
-        scanViewModel.getBarcodeNumber().observe(viewLifecycleOwner, Observer {
-                t->
-            if(t!=null){
-                Log.d("TAG",t)
-                barcodeET.setText(t)
-                showFoodDetailsButton.performClick()
-            }else{
-                Log.d("TAG","BRAK")
-            }
-        })
-    }
-
-    private fun setupFragment(){
+    private fun setupFragment() {
+        
         showFoodDetailsButton.setOnClickListener {
             showScanLabelAnimation()
-            Handler().postDelayed({findNavController().navigate(R.id.action_scanFragment_to_foodDetailsFragment)},4000)
+            getFoodInfo()
         }
 
         scanImage.setOnClickListener {
@@ -56,24 +56,53 @@ class ScanFragment : Fragment() {
         }
     }
 
-    private fun showScanLabelAnimation(){
-        try{
-            setAnim(scanLabel,1500,200f)
-            Handler().postDelayed({setAnim(scanLabel,2000,-200f)},750)
-            Handler().postDelayed({setAnim(scanLabel,2000,200f)},1750)
-            Handler().postDelayed({setAnim(scanLabel,2000,-200f)},2750)
-        }catch (ex:Exception){
 
-        }
-
+    private fun getBarCode() {
+        scanViewModel.getBarcodeNumber().observe(viewLifecycleOwner, Observer { t ->
+            if (t != null) {
+                Log.d("TAG", t)
+                barcodeET.setText(t)
+                showFoodDetailsButton.performClick()
+            } else {
+                Log.d("TAG", "BRAK")
+            }
+        })
     }
 
 
-    private fun setAnim(v:View,durationTime:Long,value:Float){
+    private fun showScanLabelAnimation() {
+        try {
+            setAnim(scanLabel, 1500, 200f)
+            handler.postDelayed({ setAnim(scanLabel, 2000, -200f) }, 750)
+            handler.postDelayed({ setAnim(scanLabel, 2000, 200f) }, 1750)
+            handler.postDelayed({ setAnim(scanLabel, 2000, -200f) }, 2750)
+        } catch (ex: Exception) {
+        }
+    }
+
+
+    private fun setAnim(v: View, durationTime: Long, value: Float) {
         ObjectAnimator.ofFloat(v, "translationY", value).apply {
             duration = durationTime
             start()
         }
     }
 
+
+    private fun getFoodInfo() {
+        try {
+            CoroutineScope(Dispatchers.IO).launch {
+                food = RetrofitClient.instance.getFoodAsync().await().body()!!
+                Log.d("TAG", food.product.productName)
+                requireActivity().runOnUiThread {
+                    handler.postDelayed({findNavController().navigate(R.id.action_scanFragment_to_foodDetailsFragment)},3000)
+                }
+            }
+        }catch (ex:Exception){ }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacksAndMessages(null)
+    }
 }
