3042412fd5edd1c6b23fd99a6a7b546a9f0a7b83
andryue <kacperwojak17@gmail.com>
2020-06-24 15:38:25 +0200

    final touhches

--- app/src/main/java/com/example/foodpoint/screens/FoodDetailsFragment.kt
+++ app/src/main/java/com/example/foodpoint/screens/FoodDetailsFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.foodpoint.R
+import com.example.foodpoint.api.food_class.food_class_to_communicate_wwith_others.SimplyfiFood
 import com.example.foodpoint.screens.view_models.FoodInfoViewModel
 import kotlinx.android.synthetic.main.fragment_food_details.*
 
@@ -18,29 +19,29 @@ class FoodDetailsFragment : Fragment() {
 
 
     private lateinit var foodInfoViewModel: FoodInfoViewModel
-
+    private lateinit var food:SimplyfiFood
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_food_details, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         foodInfoViewModel = ViewModelProvider(requireActivity()).get(FoodInfoViewModel::class.java)
         foodInfoViewModel.getFood().observe(viewLifecycleOwner, Observer {
             t->
-            Log.d("TAG",t.name.toString())
-            Log.d("TAG",t.calories.toString())
-            Log.d("TAG",t.carbohydrates.toString())
-            Log.d("TAG",t.allergens.toString())
-            Log.d("TAG",t.categories.toString())
-            Log.d("TAG",t.fats.toString())
-            Log.d("TAG",t.ingredients.toString())
+            food = t
+            setFoodInfo()
         })
+
         setupNavigation()
     }
 
-    private fun setupNavigation(){
+    private fun setFoodInfo(){
 
+    }
+
+    private fun setupNavigation(){
         backToHomeButton.setOnClickListener {
             findNavController().navigate(R.id.action_foodDetailsFragment_to_homeFragment)
         }
