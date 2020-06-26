367774abd223ca6fd96742fe3a5cf3027030726b
andryue <kacperwojak17@gmail.com>
2020-06-26 13:15:09 +0200

    set food information in recycler view

--- app/src/main/java/com/example/foodpoint/screens/adapters/recycler_views/HistoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/foodpoint/screens/adapters/recycler_views/HistoryRecyclerViewAdapter.kt
@@ -6,6 +6,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodpoint.R
 import com.example.foodpoint.api.food_class.food_class_to_communicate_wwith_others.SimplyfiFood
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_food_details.view.*
+import kotlinx.android.synthetic.main.history_card.view.*
 
 class HistoryRecyclerViewAdapter(private val listOfFoodHistory : ArrayList<SimplyfiFood>):RecyclerView.Adapter<HistoryViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
@@ -17,10 +20,16 @@ class HistoryRecyclerViewAdapter(private val listOfFoodHistory : ArrayList<Simpl
     }
 
     override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        holder.foodName.text = listOfFoodHistory[holder.adapterPosition].name
+        Picasso.get().load(listOfFoodHistory[holder.adapterPosition].imageUrl).into(holder.foodImage)
     }
 
 
 }
 class HistoryViewHolder(view :View) :RecyclerView.ViewHolder(view){
-
+    val foodName = view.historyFoodName!!
+    val foodBarcodeNumber = view.historyFoodNumber!!
+    val foodImage = view.historyFoodImage!!
+    val deleteButton = view.historyDeleteIcon!!
+    val checkButton = view.historyCheckIcon!!
 }
