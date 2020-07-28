857db11fc48cd21e54c4d47475922be49939dac5
andryue <kacperwojak17@gmail.com>
2020-07-28 09:26:49 +0200

    set app language on choose it in dialog

--- app/src/main/java/com/example/foodpoint/dialogs/ListViewDialog.kt
+++ app/src/main/java/com/example/foodpoint/dialogs/ListViewDialog.kt
@@ -4,11 +4,13 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.DialogFragment
 import com.example.foodpoint.R
-import java.lang.Exception
+import java.util.*
 
 class ListViewDialog(private val sharedPrefs:SharedPreferences,private val title: String, private val items: Array<String>,private val chooseLanguage:String) :
     DialogFragment() {
@@ -19,14 +21,31 @@ class ListViewDialog(private val sharedPrefs:SharedPreferences,private val title
                     items,
                     items.indexOf(chooseLanguage),
                     DialogInterface.OnClickListener { dialogInterface, i ->
-                        try {
+                        Log.d("NUMER",i.toString())
+                            when(i){
+                                0->setLang("de")
+                                1->setLang("en")
+                                2->setLang("pl")
+                            }
                             sharedPrefs.edit().apply {
                             putString("lang",items[i])
                             apply()
                         }
                             Handler().postDelayed( {dialogInterface.dismiss()},200)
-                        }catch (ex:Exception){ }
                     }).create()
         }
     }
+
+
+    @Suppress("DEPRECATION")
+    private fun setLang(countryCode:String){
+        val myLocale = Locale(countryCode)
+        val res = resources
+        val dm = res.displayMetrics
+        val conf = res.configuration
+        conf.locale = myLocale
+        res.updateConfiguration(conf, dm)
+        requireActivity().onBackPressed()
+    }
 }
+
